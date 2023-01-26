6e801327d88e562a4baa4fea9a45a43043a2af48
Elizaveta Slonova <pufliza@gmail.com>
2023-01-26 17:32:10 +0300

    The formatting of the playing field was done. Added a scoring block

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ SIZE_BLOCK = 20
 FRAME_COLOR = (223, 185, 136)
 SQUARE_COLOR = (246, 240, 212)
 SQUARE_COLOR_2 = (255, 255, 255)
+HEADER_COLOR = (200, 160, 110)
 COUNT_BLOCKS = 20
 MARGIN = 1
-size = [500, 600]
+HEADER_SIZE = 70
+size = [SIZE_BLOCK*COUNT_BLOCKS+2*COUNT_BLOCKS+MARGIN*COUNT_BLOCKS, SIZE_BLOCK*COUNT_BLOCKS+2*COUNT_BLOCKS+MARGIN*COUNT_BLOCKS + HEADER_SIZE]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Snake')
 
@@ -16,6 +18,7 @@ while True:
             pygame.quit()
 
     screen.fill(FRAME_COLOR)
+    pygame.draw.rect(screen, HEADER_COLOR, [0, 0, size[0], HEADER_SIZE])
 
     for row in range(COUNT_BLOCKS):
 
@@ -24,8 +27,8 @@ while True:
                 color = SQUARE_COLOR
             else:
                 color = SQUARE_COLOR_2
-            pygame.draw.rect(screen, color, [10+column*SIZE_BLOCK+MARGIN*(column+1),
-                                             20+row*SIZE_BLOCK+MARGIN*(row+1),
+            pygame.draw.rect(screen, color, [SIZE_BLOCK+column*SIZE_BLOCK+MARGIN*(column+1),
+                                             HEADER_SIZE+COUNT_BLOCKS+row*SIZE_BLOCK+MARGIN*(row+1),
                                              SIZE_BLOCK,
                                              SIZE_BLOCK])
     pygame.display.flip()
