75d25ca9801523957ef818c5a640e97b3da9c7f6
Elizaveta Slonova <pufliza@gmail.com>
2023-01-26 14:01:27 +0300

    create playing field

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
 import random
-
-size = [600, 800]
+SIZE_BLOCK = 20
+FRAME_COLOR = (223, 185, 136)
+SQUARE_COLOR = (246, 240, 212)
+SQUARE_COLOR_2 = (255, 255, 255)
+COUNT_BLOCKS = 20
+MARGIN = 1
+size = [500, 600]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Snake')
 
@@ -9,3 +14,18 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+    screen.fill(FRAME_COLOR)
+
+    for row in range(COUNT_BLOCKS):
+
+        for column in range(COUNT_BLOCKS):
+            if (row+column) % 2 == 0:
+                color = SQUARE_COLOR
+            else:
+                color = SQUARE_COLOR_2
+            pygame.draw.rect(screen, color, [10+column*SIZE_BLOCK+MARGIN*(column+1),
+                                             20+row*SIZE_BLOCK+MARGIN*(row+1),
+                                             SIZE_BLOCK,
+                                             SIZE_BLOCK])
+    pygame.display.flip()
